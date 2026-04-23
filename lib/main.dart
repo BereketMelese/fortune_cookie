a71b75aa2b4ff0b28d6b9d069f56f7ad796c88ca
BereketMelese <bereketmeles00@gmail.com>
2026-04-23 14:38:24 +0300

    feat: Add Windows support for fortune_cookie application
    - Created CMakeLists.txt for Windows build configuration.
    - Added manifest.json for web application settings.
    - Implemented Windows runner files including main application logic, window management, and resource handling.
    - Introduced utility functions for console management and command line argument parsing.
    - Added resource files for application icon and versioning.
    - Configured .gitignore for Windows-specific files.
    - Established plugin registration mechanism for Flutter plugins.

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+  String currentFortune = "";
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _currentFortune = "";
+
+  final _fortuneList = [
+    "You will find a new friend",
+    "You will find a new friend tomorrow",
+    "A truly rich life contains love and art",
+    "A faithful friend is a strong defense",
+    "A fresh start will put you on your way",
+    "A friend asks only for your time not your money",
+  ];
+
+  void _randomFortune() {
+    var random = Random();
+    int fortune = random.nextInt(_fortuneList.length);
+
+    setState(() {
+      _currentFortune = _fortuneList[fortune];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Flutter Demo Home Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  _currentFortune,
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _randomFortune,
+              child: Text("Get Fortune"),
+            ),
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _randomFortune,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
+    );
+  }
+}
